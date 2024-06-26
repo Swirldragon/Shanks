e9e5c77f4c3d8cba3b02dc3e139d6e3af905679a
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 17:57:06 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from config import env_vars
 from aiohttp import web
 import web_server
 
@@ -8,10 +7,14 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
+API_ID = 18208497
+API_HASH = "b9f8cdba86d3406944419974334e34d5"
+BOT_TOKEN = "5170934938:AAHWTgb6FH676fzS-JI3lxxomESplXS8ai0"
+
 Bot = Client('Bot',
-             api_id=18208497,
-             api_hash=b9f8cdba86d3406944419974334e34d5,
-             bot_token=5170934938:AAHWTgb6FH676fzS-JI3lxxomESplXS8ai0,
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
              plugins={
                 "root": "plugins"
             }
