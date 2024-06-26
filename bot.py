43a47ed78a5327e6df9fc2544b870c4f151db655
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 14:52:18 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,13 @@ from config import API_HASH, API_ID, LOGGER, BOT_TOKEN, PORT
 
 from pyrogram import Client
 
-app = Client("my_bot", 
-             api_id=API_ID, 
-             api_hash=API_HASH, 
-             bot_token=BOT_TOKEN)
+class Bot(Client):
+    def __init__(self):
+        super().__init__("my_bot",
+                         api_id=API_ID,
+                         api_hash=API_HASH, 
+                         bot_token=BOT_TOKEN)
+      
 
 app.start()
 
