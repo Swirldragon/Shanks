2a7be0c7a0f8736f5252daeec7bdf9db65907281
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 12:31:49 +0545

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
-import telegram
+import loguru
+
+loguru.logger.add('telegram_bot.log', level='INFO')
 
 # Create a Telegram bot instance
-bot = telegram.Bot(token='YOUR_BOT_TOKEN')
+bot = telegram.Bot(token='5170934938:AAHWTgb6FH676fzS-JI3lxxomESplXS8ai0')
 
-# Set up logging to a Telegram chat
-logging_chat_id = 'YOUR_LOGGING_CHAT_ID'
-bot.send_message(chat_id=logging_chat_id, text='Bot started')
+# Log a message
+loguru.logger.info('Bot started')
 
 # Handle updates
 def handle_update(update):
-    bot.send_message(chat_id=logging_chat_id, text=f'Received update: {update}')
+    loguru.logger.info(f'Received update: {update}')
 
 bot.start_polling()
