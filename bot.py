80f867547667d618a29599c15595faaae81af8fb
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 12:40:36 +0545

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from config import API_HASH, APP_ID, LOGGER, BOT_TOKEN, PORT
+from datetime import datetime
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={
+                "root": "plugins"
+            },
+            bot_token=BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
