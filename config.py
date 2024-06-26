e10f572c40f2d03203210067bdb0e26c91921ef6
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 18:02:50 +0545

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-env_file = "env.json"
-if os.path.exists(env_file):
-    with open(env_file) as f:
-        env_vars = json.loads(f.read())
-else:
-    env_vars = dict(os.environ)
