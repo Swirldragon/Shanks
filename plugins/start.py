65061a0234f933c40f15ea9cbeece8eacecf8859
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 13:56:13 +0545

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -43,5 +43,5 @@ async def start_command(client: Client, message: Message):
     username = user.username
     name = user.first_name
     await message.reply(photo=image, caption=f"Hey [{name}](https://t.me/{username})\n\nI'm A Multi-Function Bot, i can rename files and can do many useful stuff click on Help button to know my secrets",
-                           reply_markup=InlineKeyboardMarkup(buttons),
-                          )
+                        reply_markup=InlineKeyboardMarkup(buttons),
+                        )                  
