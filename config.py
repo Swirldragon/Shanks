aa0b20e96454b40742e8b88b4fe694c5d0392afe
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 16:12:58 +0545

    Update config.py

--- config.py
+++ config.py
@@ -1,43 +1,9 @@
 import os
-import logging
-from logging.handlers import RotatingFileHandler
-from dotenv import load_dotenv
+import json
 
-#Bot token @Botfather
-BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5170934938:AAHWTgb6FH676fzS-JI3lxxomESplXS8ai0")
-
-#Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "20631875"))
-
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "e770f81e0d2f21d48f738c010a51fd6e")
-
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001550448024"))
-
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "1880221341"))
-
-#Port
-PORT = os.environ.get("PORT", "8080")
-
-LOG_FILE_NAME = "renamerbot.txt"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+env_file = "env.json"
+if os.path.exists(env_file):
+    with open(env_file) as f:
+        env_vars = json.loads(f.read())
+else:
+    env_vars = dict(os.environ)
