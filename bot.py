e26812a8c21e4f42406fbf8c5ae1306dfe6f9448
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-26 14:37:30 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,9 @@
-import pyromod.listen
-from pyrogram import Client
-from pyrogram.enums import ParseMode
-import sys
-from datetime import datetime
-
-from aiohttp import web
-import web_server
 from config import API_HASH, APP_ID, LOGGER, BOT_TOKEN, PORT
 
-class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            name="Bot",
-            api_hash=API_HASH,
-            api_id=APP_ID,
-            plugins={
-                "root": "plugins"
-            },
-            bot_token=BOT_TOKEN
-        )
-        self.LOGGER = LOGGER
+from pyrogram import Client, BotToken
 
-    async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.uptime = datetime.now()
-        
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!")
-        self.LOGGER(__name__).info(f"Bot Run")
-        self.username = usr_bot_me.username
-        #web-response
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
- 
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BotToken(BOT_TOKEN))
+
+app.start()
+
+app.stop()
