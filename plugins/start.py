dd3af4e9459277a184cd795c78ca047bb942bca4
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-27 09:22:08 +0545

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,18 +4,19 @@ from pyrogram import Client, filters
 import pyrogram.errors
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaDocument
 
-photos = ( "https://graph.org/file/e93290b99f1a87211ee7c.jpg",
-           "https://graph.org/file/ccf9d8a22e8220990850d.jpg",
-           "https://graph.org/file/5e9f4d6b735a19e973d12.jpg",
-           "https://graph.org/file/e358de4b9807c4c5a1cd6.jpg",
-           "https://graph.org/file/4523cccaf531c8fcbdc79.jpg",)
+photos = ( "https://graph.org/file/0c85bf7ce08bbb2bdd7b4.jpg",
+           "https://graph.org/file/f99c470a76c65a1e1232d.jpg",
+           "https://graph.org/file/ffbc541990d0489cbb538.jpg",
+           "https://graph.org/file/4ac10607462346840a323.jpg",
+           "https://graph.org/file/c3baa04ca9365e4e11dac.jpg",)
 
 START_TEXT = "Hey {}!\n\nI'm A Multi-Function Bot, i can rename files and can do many useful stuff click on Help button to know my secrets."
 ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅"
+GROUP_TEXT = "<b>Hey {}!\n\nShanks here, use [/help](https://t.me/ShanksXRobot?start=help) to know my cmds.</b>"
 
 button = [[        
-        InlineKeyboardButton('😊 Help', callback_data = "help"),
-        InlineKeyboardButton("🔒 Settings", callback_data = "setting")
+        InlineKeyboardButton('* Help *', url= "https://t.me/ShanksXRobot?start=help"),
+        InlineKeyboardButton("* Settings *", callback_data = "setting")
     ]]
 keyboard = InlineKeyboardMarkup([[button]])
 
@@ -37,3 +38,9 @@ async def req_accept(client: Client, message: Message):
     await client.approve_chat_join_request(chat_id, user_id)
     try: await client.send_message(user_id, ACCEPTED_TEXT.format(user=message.from_user.mention, chat=message.chat.title))
     except Exception as e: print(e)
+
+
+@Bot.on_message(filters.command("start") & filters.group) 
+async def start_process(client: Client, message: Message):
+           image = random.choice(photos)
+           await message.reply_photo(photo=image, caption=GROUP_TEXT.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(button), reply_to_message_id=message.from_user.id)
