8d5dea7d61cf439a640f432e5dc4a1e91d990191
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-30 18:38:16 +0545

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -19,24 +19,4 @@ setting_b = [
 async def on_setting(client: Client, message: Message):
     image = random.choice(photos)
     await client.send_photo(chat_id=message.from_user.id, photo=image, caption="List of Setting:", reply_markup=InlineKeyboardMarkup(setting_b))
-
-@Bot.on_callback_query()
-async def cb_handler(client: Bot, query: CallbackQuery):
-    data = query.data
-    if data == "rename":
-        await query.message.edit_text(
-            text = "Your Rename Settings:",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                        [InlineKeyboardButton("UPLOAD AS DOCUMENT", callback_data = "upload_as_doc")],
-                        [InlineKeyboardButton("APPLY CAPTION", callback_data = "ApplyDefaultCaption")],
-                        [InlineKeyboardButton("𝚂𝙴𝚃 𝙲𝚄𝚂𝚃𝙾𝙼 𝙲𝙰𝙿𝚃𝙸𝙾𝙽", callback_data = "setCustomCaption")],
-                        [InlineKeyboardButton("𝚂𝙴𝚃 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻", callback_data = "setThumbnail")],
-                        [InlineKeyboardButton("MEGA EMAIL", callback_data = "megaemail")],
-                        [InlineKeyboardButton("MEGA PASSWORD", callback_data = "megapass")],
-                ]
-            )
-        )
-        
-                        
+                     
