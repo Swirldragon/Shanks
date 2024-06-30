ad4ee4744086dc431d0557c6c73eccba239ee4a3
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-30 21:44:10 +0545

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -6,20 +6,21 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "help" or "back":
+    if data == "help":
           await query.message.edit_text(
-            text = "<b>List of modules:</b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                        [InlineKeyboardButton("* Rename *", callback_data = "ab_rename")],
-                        [InlineKeyboardButton("* Request Approval *", callback_data = "ra")],
-                        [InlineKeyboardButton("* PDf *", callback_data = "pdf")],
-                        [InlineKeyboardButton("* Converter *", callback_data = "converter")],
-                        [InlineKeyboardButton("Close", callback_data = "close")],
-                ]
-            )
-        )
+                text = "<b>List of modules:</b>",
+                disable_web_page_preview = True,
+                reply_markup = InlineKeyboardMarkup(
+                      [
+                            [InlineKeyboardButton("* Rename *", callback_data = "ab_rename")],
+                            [InlineKeyboardButton("* Request Approval *", callback_data = "ra")],
+                            [InlineKeyboardButton("* PDf *", callback_data = "pdf")],
+                            [InlineKeyboardButton("* Converter *", callback_data = "converter")],
+                            [InlineKeyboardButton("Close", callback_data = "close")],
+                      ]
+                )
+          )
+
     elif data == "ab_rename":
           await query.message.edit_text(
                 text = "<b>☆ Rename Function:\n\nWe Currently Have Three Modes In Our Bot..\n\n•Auto Mode : This Will Rename Your Files Auto With using <code>/rename</code> Function.\n\n• Manual: This Is Doned By <code>/rename Ch-123 @Manga_Arena.pdf</code>.\n\n•Mega: This Rename The Files At Mega Account Folders. It Can Rename Whole Files In Folder at Time. Use <code>/mega</code>.\n\n To Set Caption, Thumbnail, Mega Email and Password Use <code>/setting</code>\n\nMade By @Wizard_Bots.</b>",
@@ -46,21 +47,21 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                              
     elif data == "rename": 
           await query.message.edit_text(
-            text = "<b>Your Rename Settings:</b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                        [InlineKeyboardButton("UPLOAD AS DOCUMENT", callback_data = "upload_as_doc")],
-                        [InlineKeyboardButton("CAPTION", callback_data = "setCustomCaption")],
-                        [InlineKeyboardButton("THUMBNAIL", callback_data = "setThumbnail")],
-                        [InlineKeyboardButton("MEGA EMAIL", callback_data = "megaemail")],
-                        [InlineKeyboardButton("MEGA PASSWORD", callback_data = "megapass")],
-                        [InlineKeyboardButton("AUTO RENAME", callback_data = "auto_rename")],
-                        [InlineKeyboardButton("CLOSE", callback_data = "close")],
-                ]
-            )
-        )
-        
+                text = "<b>Your Rename Settings:</b>",
+                disable_web_page_preview = True,
+                reply_markup = InlineKeyboardMarkup(
+                      [
+                            [InlineKeyboardButton("UPLOAD AS DOCUMENT", callback_data = "upload_as_doc")],
+                            [InlineKeyboardButton("CAPTION", callback_data = "setCustomCaption")],
+                            [InlineKeyboardButton("THUMBNAIL", callback_data = "setThumbnail")],
+                            [InlineKeyboardButton("MEGA EMAIL", callback_data = "megaemail")],
+                            [InlineKeyboardButton("MEGA PASSWORD", callback_data = "megapass")],
+                            [InlineKeyboardButton("AUTO RENAME", callback_data = "auto_rename")],
+                            [InlineKeyboardButton("CLOSE", callback_data = "close")],
+                      ]
+                )
+          )
+     
     elif data == "upload_as_doc":
           pass
     elif data == "setCustomCaption":
@@ -73,11 +74,25 @@ async def cb_handler(client: Bot, query: CallbackQuery):
           pass
     elif data == "auto_rename":
           pass
-        
+    elif data == "help":
+          await query.message.edit_text(
+                text = "<b>List of modules:</b>",
+                disable_web_page_preview = True,
+                reply_markup = InlineKeyboardMarkup(
+                      [
+                            [InlineKeyboardButton("* Rename *", callback_data = "ab_rename")],
+                            [InlineKeyboardButton("* Request Approval *", callback_data = "ra")],
+                            [InlineKeyboardButton("* PDf *", callback_data = "pdf")],
+                            [InlineKeyboardButton("* Converter *", callback_data = "converter")],
+                            [InlineKeyboardButton("Close", callback_data = "close")],
+                      ]
+                )
+          )
+          
     elif data == "close":
-        await query.message.delete()
-        try:
-            await query.message.reply_to_message.delete()
-        except:
-            pass
+          await query.message.delete()
+          try:
+                await query.message.reply_to_message.delete()
+          except:
+                pass
   
