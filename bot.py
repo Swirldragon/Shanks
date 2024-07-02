b65de8547757f537880b62f309261305607ecec7
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-07-02 16:09:40 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
-
+from aiohttp import web
+from plugins import web_server
 
 API_ID = 18208497
 API_HASH = "b9f8cdba86d3406944419974334e34d5"
@@ -8,3 +9,28 @@ LOG_CHANNEL = -1001885135958
 DB_URL = "mongodb+srv://shankswillson33:shankswillson33@cluster0.vshfo6u.mongodb.net/?retryWrites=true&w=majority"
 
 Bot = Client(name='Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,  plugins={"root": "plugins"})
+
+class Bot(Client):
+    def __init__(self):
+      super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={
+                "root": "plugins"
+            }
+        )
+      
+    async def start(self):
+      await super().start()
+      usr_bot_me = await self.get_me()
+      self.uptime = datetime.now()
+      #web-response
+      app = web.AppRunner(await web_server())
+      await app.setup()
+      bind_address = "0.0.0.0"
+      await web.TCPSite(app, bind_address, PORT).start()
+      
+    async def stop(self, *args):
+      await super().stop()
+      self.LOGGER(__name__).info("Bot stopped.")
