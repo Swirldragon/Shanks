5bf0413fa18d0ccf5b9413803002c78263c8f26b
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-07-03 14:54:14 +0545

    Update add.py

--- database/add.py
+++ database/add.py
@@ -7,8 +7,3 @@ from pyrogram.types import Message
 async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
-        if LOG_CHANNEL is not None:
-            await bot.send_flooded_message(
-                int(LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
-            )
