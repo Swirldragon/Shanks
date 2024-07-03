c2e6efcf24796e180656bab2d086cae255be7dce
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-07-03 14:42:16 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ API_ID = 18208497
 API_HASH = "b9f8cdba86d3406944419974334e34d5"
 BOT_TOKEN = "5170934938:AAHWTgb6FH676fzS-JI3lxxomESplXS8ai0"
 LOG_CHANNEL = -1001885135958
-DB_URL = "mongodb+srv://justatestsubject01:<password>@cluster0.wizfkbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_URL = "mongodb+srv://justatestsubject01:HzP5SK8ZiiLHcF3o@cluster0.wizfkbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 Bot = Client(name='Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,  plugins={"root": "plugins"})
 
