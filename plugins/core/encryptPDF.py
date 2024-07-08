22c7aaafc2ff8052b1615aa6392b5b9e9e71ae5a
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-07-08 22:00:21 +0545

    Update encryptPDF.py

--- plugins/core/encryptPDF.py
+++ plugins/core/encryptPDF.py
@@ -1,33 +1,34 @@
-from pyromod import Client, Message
-from pyrogram.types import Message as M
-from pyrogram import Client as C
+from pyromod import Chat
+from pyrogram import Client, filters
+from pyrogram.types import Message
 import PyPDF2
 from io import BytesIO
+from bot import Bot 
 
-@Client.on_message(filters.command("encryptPDF"))
-async def encrypt_pdf(client: Client, message: Message, M, C):
-    reply = M.reply_to_message
+@Bot.on_message(filters=filters.command(["encryptPDF"]))
+async def encrypt_pdf(client: Client, message: Message):
+    reply = message.reply_to_message
     if reply:
-        pdf_file = await C.download_media(reply.document)
+        pdf_file = await client.download_media(reply.document)
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         num_pages = len(pdf_reader.pages)
         
         # Ask for password
-        password_a = await chat.ask("Please enter a password to encrypt the PDF file. Type `/cancel` to cancel.")
+        password_a = await Chat.ask("Please enter a password to encrypt the PDF file. Type `/cancel` to cancel.")
         password_message = password_a.text
         
         if password_message.text == "/cancel":
             await message.reply("Encryption process cancelled.")
             return
         
-        password = password_message
+        password = password_message.text
         
         # Ask for new file name
         filename_a = await chat.ask("Please enter a new file name for the encrypted PDF file. Type `/cancel` to cancel.")
         filename_message = filename_a.text
         
         if filename_message.text == "/cancel":
-            await M.reply("Encryption process cancelled.")
+            await message.reply("Encryption process cancelled.")
             return
         
         filename = filename_message.text + ".pdf"
@@ -44,7 +45,7 @@ async def encrypt_pdf(client: Client, message: Message, M, C):
         pdf_writer.write(encrypted_pdf)
         encrypted_pdf.seek(0)
         # Reply to the original message with the encrypted PDF
-        await M.reply_to_message.reply_document(encrypted_pdf, file_name=filename)
+        await message.reply_to_message.reply_document(encrypted_pdf, file_name=filename)
 
     
     else:
