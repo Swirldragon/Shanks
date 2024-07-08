42924d5173f6cc5c3b7181f6bab8284af1b6482b
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-07-08 22:07:54 +0545

    Update encryptPDF.py

--- plugins/core/encryptPDF.py
+++ plugins/core/encryptPDF.py
@@ -1,4 +1,4 @@
-from pyromod import Chat
+from pyromod import Chat.ask as ca
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import PyPDF2
@@ -14,7 +14,7 @@ async def encrypt_pdf(client: Client, message: Message):
         num_pages = len(pdf_reader.pages)
         
         # Ask for password
-        password_a = await Chat.ask("Please enter a password to encrypt the PDF file. Type `/cancel` to cancel.")
+        password_a = await ca("Please enter a password to encrypt the PDF file. Type `/cancel` to cancel.")
         password_message = password_a.text
         
         if password_message.text == "/cancel":
@@ -24,7 +24,7 @@ async def encrypt_pdf(client: Client, message: Message):
         password = password_message.text
         
         # Ask for new file name
-        filename_a = await Chat.ask("Please enter a new file name for the encrypted PDF file. Type `/cancel` to cancel.")
+        filename_a = await ca("Please enter a new file name for the encrypted PDF file. Type `/cancel` to cancel.")
         filename_message = filename_a.text
         
         if filename_message.text == "/cancel":
