4b9fe3c02c5e120d182e52a97e8425f44d9761c5
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-07-09 17:38:39 +0545

    Update encryptPDF.py

--- plugins/core/encryptPDF.py
+++ plugins/core/encryptPDF.py
@@ -1,6 +1,6 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
-import PyPDF2
+from pypdf import PdfReader, PdfWriter
 from io import BytesIO
 from bot import Bot 
 
@@ -9,30 +9,30 @@ async def encrypt_pdf(client: Client, message: Message):
     reply = message.reply_to_message
     if reply:
         pdf_file = await client.download_media(reply.document)
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        pdf_reader = PdfReader(pdf_file)
         num_pages = len(pdf_reader.pages)
         
         # Ask for password
-        await message.reply("Please enter a password to encrypt the PDF file. Type `/cancel` to cancel.")
-        password_message =  await Bot.await_message()
+        #await message.reply("Please enter a password to encrypt the PDF file. Type `/cancel` to cancel.")
+        #password_message =  await Bot.await_message()
         
-        if password_message.text == "/cancel":
-            await message.reply("Encryption process cancelled.")
-            return
+        #if password_message.text == "/cancel":
+            #await message.reply("Encryption process cancelled.")
+            #return
         
-        password = password_message.text
+        password = "@M"
         
         # Ask for new file name
-        filename_message = await c.ask("Please enter a new file name for the encrypted PDF file. Type `/cancel` to cancel.")
+        #filename_message = await c.ask("Please enter a new file name for the encrypted PDF file. Type `/cancel` to cancel.")
         
-        if filename_message.text == "/cancel":
-            await message.reply("Encryption process cancelled.")
-            return
+        #if filename_message.text == "/cancel":
+            #await message.reply("Encryption process cancelled.")
+            #return
         
-        filename = filename_message.text + ".pdf"
+        filename = "ch-100" + ".pdf"
         
         # Encrypt the PDF file
-        pdf_writer = PyPDF2.PdfWriter()
+        pdf_writer = PdfWriter()
         
         for page in pdf_reader.pages:
             pdf_writer.add_page(page)
