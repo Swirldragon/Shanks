3bd4a17c48560b557f010e79c378ce1fbbc4f7ac
justatest420 <justatestsubject.01@gmail.com>
2024-07-16 12:56:52 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,29 +11,4 @@ LOG_CHANNEL = -1001885135958
 DB_URL = "mongodb+srv://justatestsubject01:HzP5SK8ZiiLHcF3o@cluster0.wizfkbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 PORT = 8080
 
-class Bot(Client):
-  def __init__(self):
-    super().__init__(
-      name="Shanks",
-      api_id=API_ID,
-      api_hash=API_HASH,
-      bot_token=BOT_TOKEN,
-      plugins={"root": "plugins"},
-      sleep_threshold=10,
-    )
-    
-  async def start(self):
-    await super().start()
-    me = await self.get_me()      
-    print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
-    #web-response
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-    
-  async def stop(self, *args):
-    await super().stop()      
-    print("Bot Restarting........")
-    
-
+Bot = Client(name='Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,  plugins={"root": "plugins"})
