09b8a93b8028b3f9cb12a11439637d4b4c6a512a
justatest420 <justatestsubject.01@gmail.com>
2024-07-16 13:11:17 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from bot import Bot
+from aiohttp import web
+from plugins import web_server
+
+#web-response
+app = web.AppRunner(await web_server())
+await app.setup()
+bind_address = "0.0.0.0"
+await web.TCPSite(app, bind_address, PORT).start()
 
 Bot.run()
