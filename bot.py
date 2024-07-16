7cdaeff3d9804c8959c63ba4df331ee6e73e772f
justatest420 <justatestsubject.01@gmail.com>
2024-07-16 13:39:32 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,4 +11,4 @@ LOG_CHANNEL = -1001885135958
 DB_URL = "mongodb+srv://justatestsubject01:HzP5SK8ZiiLHcF3o@cluster0.wizfkbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 PORT = 8080
 
-Bot = Client(name='Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,  plugins={"root": "plugins"})
+Bot = Client(name='Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
