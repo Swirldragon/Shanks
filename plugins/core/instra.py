846ab58a11732af248c94843cdd78958e663676a
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 12:49:24 +0545

    Update instra.py

--- plugins/core/instra.py
+++ plugins/core/instra.py
@@ -9,11 +9,13 @@ import json
 @Bot.on_message(filters.text & filters.private)
 async def instra_reels(client: Client, message: Message):
   if "https://www.instagram.com/" in message.text:
+    user_id = messsage.from_user.id
     msg = await message.reply("Featching The Url")
     urls = message.text
     cget = create_scraper().request
     rjson = cget('GET', f'https://insta-dl.hazex.workers.dev/?url={urls}').json()
-    vlink = rjson["url"]
+    vlinks = rjson["result"]
+    vlink = vlinks["url"]
     downloaded = await client.download(vlink)
     await bot.send_video(message.from_user.id, video=downloaded)
     msg.delete
