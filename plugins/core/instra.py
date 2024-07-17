d191de2f31f315977c25edc0f4c583f522c76200
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 09:41:20 +0545

    Update instra.py

--- plugins/core/instra.py
+++ plugins/core/instra.py
@@ -2,21 +2,18 @@ from cloudscraper import create_scraper
 from bot import Bot
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+import json
 
 # url = "https://insta-dl.hazex.workers.dev/?url=https://instagram.com/reel/ABC"
 
-@Bot.on_message(filters.private & filters.text & filters.regex(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'))
+@Bot.on_message(filters.text & filters.private)
 async def instra_reels(client: Client, message: Message):
-  await message.reply("Featching The Url")
-  urls = message.text
-  try:
-    
+  if "https://instagram.com/" in message.text:
+    await message.reply("Featching The Url")
+    urls = message.text
     cget = create_scraper().request
     rjson = cget('GET', f'https://insta-dl.hazex.workers.dev/?url={urls}').json()
     vlink = rjson["url"]
     downloaded = await client.download(vlink)
     await bot.send_video(message.from_user.id, video=downloaded)
     
-  except:
-    await message.reply_text("Sorry! There Problem at Link. Check Link is Public Or Private.")
-  
