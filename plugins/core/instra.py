5db5f24a67ec62c6331e26f17ad524fc4e38cb6f
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 17:58:44 +0545

    Update instra.py

--- plugins/core/instra.py
+++ plugins/core/instra.py
@@ -12,14 +12,13 @@ import time
 async def instra_reels(client: Client, message: Message):
   if "https://www.instagram.com/" in message.text:
     user_id = message.from_user.id
-    await message.reply("<b>Featching The Url....</b>")
+    await message.reply("<code>Featching The Url....</code>")
     urls = message.text
     cget = create_scraper().request
     rjson = cget('GET', f'https://insta-dl.hazex.workers.dev/?url={urls}').json()
-    
+  
     vlinks == rjson["result"]
     vlink = vlinks["url"]
     caption = "<b>Doned By @ShanksXRobot</b>"
     await client.send_video(user_id, vlink, caption=caption)
-      
     await message.reply_text("<b>Check Link is Public or Private or story. I cannot send you private reels and story.</b>")
