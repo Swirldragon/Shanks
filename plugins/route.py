07c5616ab4051d3218877a022c1446e55605da21
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 14:16:05 +0545

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Anime_Wide")
+    return web.json_response("Hey Guys Fuck U")
 
 
 async def web_server():
