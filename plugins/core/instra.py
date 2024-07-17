116e56a8263ff8122efc834214b2cf4fa049c041
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 14:19:57 +0545

    Update instra.py

--- plugins/core/instra.py
+++ plugins/core/instra.py
@@ -19,12 +19,6 @@ async def instra_reels(client: Client, message: Message):
     rjson = cget('GET', f'https://insta-dl.hazex.workers.dev/?url={urls}').json()
     vlinks = rjson["result"]
     vlink = vlinks["url"]
-    sts = await msg.reply_text("Trying to Downloading.....")
-    c_time = time.time()
-    cap = "Made By @Wizard_bots"
-    downloaded = await reply.download(file_name=cap, progress=progress_for_pyrogram, progress_args=("Download Started.....", sts, c_time))
-    await sts.edit("Trying to Uploading")
-    c_time = time.time()
-    await bot.send_video(user_id, video=downloaded, caption=cap, progress=progress_for_pyrogram, progress_args=("Uploade Started.....", sts, c_time))
+    await client.send_video(user_id, vlink)
     msg.delete()
     
