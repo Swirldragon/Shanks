298eb8e67bba0dc978e8137d312229479906a503
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 13:53:17 +0545

    Update instra.py

--- plugins/core/instra.py
+++ plugins/core/instra.py
@@ -2,6 +2,7 @@ from cloudscraper import create_scraper
 from bot import Bot
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from .utils import progress_for_pyrogram, humanbytes, convert
 import json
 
 # url = "https://insta-dl.hazex.workers.dev/?url=https://instagram.com/reel/ABC"
@@ -16,8 +17,12 @@ async def instra_reels(client: Client, message: Message):
     rjson = cget('GET', f'https://insta-dl.hazex.workers.dev/?url={urls}').json()
     vlinks = rjson["result"]
     vlink = vlinks["url"]
+    sts = await msg.reply_text("Trying to Downloading.....")
+    c_time = time.time()
     cap = "Made By @Wizard_bots"
-    downloaded = await client.download(vlink)
-    await bot.send_video(message.from_user.id, video=downloaded, caption=cap)
-    msg.delete
+    downloaded = await reply.download(file_name=cap, progress=progress_for_pyrogram, progress_args=("Download Started.....", sts, c_time))
+    await sts.edit("Trying to Uploading")
+    c_time = time.time()
+    await bot.send_video(msg.chat.id, video=downloaded, caption=cap, progress=progress_for_pyrogram, progress_args=("Uploade Started.....", sts, c_time))
+    msg.delete()
     
