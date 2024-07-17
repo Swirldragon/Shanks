71ee32acf74f6a20d59a99ae6b54c53b1ae27a6d
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 09:36:06 +0545

    Create admin.py

---
+++ plugins/admin.py
@@ -0,0 +1,61 @@
+import os
+import asyncio
+from bot import Bot, ADMINS
+from pyrogram import Client, filters, __version__
+from pyrogram.types import Message
+from database.db import db
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
+
+WAIT_MSG = """"<b>Processing ...</b>"""
+
+@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
+async def get_users(client: Client, message: Message):
+  users = await db.total_users_count()
+  await message.reply(f"{users} users are using this bot")
+
+
+@Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
+async def send_text(client: Bot, message: Message):
+    if message.reply_to_message:
+        query = await get_all_users()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+        
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        for chat_id in query:
+            try:
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except UserIsBlocked:
+                await del_user(chat_id)
+                blocked += 1
+            except InputUserDeactivated:
+                await del_user(chat_id)
+                deleted += 1
+            except:
+                unsuccessful += 1
+                pass
+            total += 1
+        
+        status = f"""<b><u>Broadcast Completed</u>
+
+Total Users: <code>{total}</code>
+Successful: <code>{successful}</code>
+Blocked Users: <code>{blocked}</code>
+Deleted Accounts: <code>{deleted}</code>
+Unsuccessful: <code>{unsuccessful}</code></b>"""
+        
+        return await pls_wait.edit(status)
+
+    else:
+        msg = await message.reply("<code>Use this command as a replay to any telegram message with out any spaces.</code>")
+        await asyncio.sleep(8)
+        await msg.delete()
