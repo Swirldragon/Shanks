70cf4d6147601dd87cd02145e9c0a81df024501d
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 14:25:25 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,6 @@ class Bot(Client):
        api_id=API_ID,
         api_hash=API_HASH,
         bot_token=BOT_TOKEN,
-        plugins=dict(root="plugins"),
         workers=50,
         sleep_threshold=10
     )
