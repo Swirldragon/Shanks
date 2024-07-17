fd8028ea32f9d9073424456df2a239a9411f2052
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 08:58:40 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,4 +11,31 @@ LOG_CHANNEL = -1001885135958
 DB_URL = "mongodb+srv://justatestsubject01:HzP5SK8ZiiLHcF3o@cluster0.wizfkbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 PORT = 8080
 
-Bot = Client(name='Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+class Bot(Client):
+  
+  def __init__(self):
+    super().__init__(
+      "Shanks",
+       api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="plugins"),
+        workers=50,
+        sleep_threshold=10
+    )
+    #web-response
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app, bind_address, PORT).start()
+    
+  async def start(self):
+    
+    await super().start()
+    print('Bot Is Start')
+
+  async def stop(self, *args):
+    
+    await super().stop()
+      print('HHHHH')
+
