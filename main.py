e0cf0445dc802722d3a2860d81ae8d2f6a08edfe
justatest420 <justatestsubject.01@gmail.com>
2024-07-17 09:23:03 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from bot import Bot
-from aiohttp import web
-from plugins import web_server
 
-async def koyeb():
-  app = web.AppRunner(await web_server())
-  await app.setup()
-  bind_address = "0.0.0.0"
-  await web.TCPSite(app, bind_address, PORT).start()
+bot = bot()
 
-Bot.run()
+bot.run()
