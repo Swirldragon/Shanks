c596a84874309529408e6b17a6ff38c1a2581920
justatest420 <justatestsubject.01@gmail.com>
2024-07-19 11:45:26 +0545

    Update rsc_save.py

--- plugins/rsc_save.py
+++ plugins/rsc_save.py
@@ -67,7 +67,7 @@ async def save(client: Client, message: Message):
     if "https://t.me/" in message.text:
         datas = message.text.split("/")
         temp = datas[-1].replace("?single","").split("-")
-        fromID = temp[0].strip()
+        fromID = int(temp[0].strip())
         try:
             toID = int(temp[1].strip())
         except:
