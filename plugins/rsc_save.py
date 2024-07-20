5efe4f7f61c0cd71abba5f32ffa3bc2c73e03418
justatest420 <justatestsubject.01@gmail.com>
2024-07-20 21:18:34 +0545

    Update rsc_save.py

--- plugins/rsc_save.py
+++ plugins/rsc_save.py
@@ -159,7 +159,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 			ph_path = None
 		try:
 			xx = await client.send_document(chat, file, thumb=ph_path, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-
+			xxx.append(xx)
 		except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
 		if ph_path != None: os.remove(ph_path)
@@ -172,7 +172,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 			ph_path = None
 		try:
 			xx = await client.send_video(chat, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=ph_path, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-
+			xxx.append(xx)
 		except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
 		if ph_path != None: os.remove(ph_path)
@@ -181,6 +181,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 	elif "Sticker" == msg_type:
 		try:
 			xx = await client.send_sticker(chat, file, reply_to_message_id=message.id)
+			xxx.append(xx)
 
 		except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
@@ -190,6 +191,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 	elif "Voice" == msg_type:
 		try:
 			xx = await client.send_voice(chat, file, caption=caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
+			xxx.append(xx)
 
 		except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
@@ -203,7 +205,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 			ph_path = None
 		try:
 			xx = await client.send_audio(chat, file, thumb=ph_path, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-
+			xxx.append(xx)
 			
 		except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
@@ -212,7 +214,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 	elif "Animation" == msg_type:
 		try:
 			xx = await client.send_animation(chat, file, reply_to_message_id=message.id)
-
+			xxx.append(xx)
 			
 		except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
@@ -222,7 +224,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 	elif "Photo" == msg_type:
 		try:
 			xx = await client.send_photo(chat, file, caption=caption, reply_to_message_id=message.id)
-
+			xxx.append(xx)
 			
 		except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
@@ -235,9 +237,9 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 	await message.reply_text("Files will be deleted After 10 seconds, Save them to the Saved Message now !!")
 	await asyncio.sleep(600)
 	
-	for snt_msg in snt_msgs:
+	for xx in xxx:
 		try:
-			await snt_msg.delete()
+			await xx.delete()
 		except:
 			pass
 
