16f55229cc35322129fe460c083b4a204293744f
justatest420 <justatestsubject.01@gmail.com>
2024-07-20 20:57:56 +0545

    Update rsc_save.py

--- plugins/rsc_save.py
+++ plugins/rsc_save.py
@@ -192,7 +192,7 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
 			xx = await client.send_voice(chat, file, caption=caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
 			xx.append(xx)
 		except Exception as e:
-			await client.send_messaif ph_path != None: os.remove(ph_path)ge(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
+			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
 		if ph_path != None: os.remove(ph_path)
 	
         elif "Audio" == msg_type:
