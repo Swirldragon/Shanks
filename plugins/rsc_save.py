c159745ca07d9411bafe278c4a3a751458769ba9
justatest420 <justatestsubject.01@gmail.com>
2024-07-20 20:09:29 +0545

    Update rsc_save.py

--- plugins/rsc_save.py
+++ plugins/rsc_save.py
@@ -183,7 +183,6 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
                 try:
 			xx = await client.send_animation(chat, file, reply_to_message_id=message.id)
 			xx.append(xx)
-			
                 except Exception as e:
 			await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
 
