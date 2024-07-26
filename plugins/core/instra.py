bd8aaf104055f618955e0546bc2316487795840b
justatest420 <justatestsubject.01@gmail.com>
2024-07-26 15:30:30 +0545

    Update instra.py

--- plugins/core/instra.py
+++ plugins/core/instra.py
@@ -17,14 +17,25 @@ async def instra_reels(client, message):
     msg = await message.reply("<code>Featching The Url....</code>")
     cget = create_scraper().request
     rjson = cget('GET', f'https://insta-dl.hazex.workers.dev/?url={urls}').json()
+    xxx = []
     try:
       vlinks = rjson["result"]
       vlink = vlinks["url"]
       caption = "<b>Doned By @ShanksXRobot</b>"
-      await client.send_video(user_id, vlink, caption=caption)
+      xx = await client.send_video(user_id, vlink, caption=caption)
       await msg.delete()
+      xxx.append(xx)
     except Exception as e:
-      exmsg = await message.reply_text("<b>Check Your Link is Private or Story.</b>")
+      xx = await message.reply_text("<b>Check Your Link is Private or Story.</b>")
+      xxx.append(xx)
       await client.send_message(-1001885135958, f"Instra Errors - <code>{e}</code>")
       await msg.delete()
-      await asyncio.sleep(6)
+    await client.delete_messages(message.chat.id,[smsg.id])
+    await message.reply_text("<code>Files will be deleted After 10 seconds, Save them to the Saved Message now !!</code>")
+    await asyncio.sleep(600)
+    
+    for xx in xxx:
+      try:
+        xx.delete()
+      except:
+        pass
